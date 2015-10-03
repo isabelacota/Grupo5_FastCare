0996aa07f1cb8cd3eb69b53681689c8e671f75af
Gabriel Hidasy Rezende <ra116928@metalica.lab.ic.unicamp.br>
2015-10-02 22:00:27 -0300

    Criada uma classe para paciente, a url base do servidor agora é /FastCare, a URL da API é /FastCare/API/*, Pacient já retorna um JSON (é só criar uma classe "Bean" (só parametros, getters, setters, e uma to_string) e responder ela como em pacient)

--- backend/src/main/java/xyz/hidasy/rest/HelloWorldService.java
+++ backend/src/main/java/xyz/hidasy/rest/HelloWorldService.java
@@ -1,24 +1,39 @@
 package xyz.hidasy.rest;
- 
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
-@Path("/paciente")
+import javax.ws.rs.QueryParam;
+
+import xyz.hidasy.rest.Pacient;
+
+@Path("/pacient")
 public class HelloWorldService {
 
     @GET
     @Path("/{id}")
-    public Response getPacient(@PathParam("id") String msg) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Pacient getPacient(@PathParam("id") String msg) {
+
+	Pacient pacient = new Pacient();
+	pacient.setName("Enter Sandman");
+	pacient.setId(msg);
 	
-	String output = "{\"status_message\": \"bla\"}";
+
+	return pacient;
 	
-	return Response.status(200).entity(output).build();
+	//String output = "{\"status_message\": \"bla\"}";
+	
+	//return Response.status(200).entity(output).build();
     }
-    
+
+    //@Consumes(MediaType.APPLICATION_JSON)
+
     @POST
     @Path("/{id}/update")
     public Response updatePacient(@PathParam("id") String msg) {
@@ -27,7 +42,8 @@ public class HelloWorldService {
 	
 	return Response.status(200).entity(output).build();
     }
-
+    
+    //@Consumes(MediaType.APPLICATION_JSON)
     @POST
     @Path("/insert")
     public Response updatePacient() {
