79012e78165cd10e79295a90feac9960cc0962b0
Caique Moreira <caique.moreira@movile.com>
2015-11-06 20:03:55 -0200

    fix on soape service

--- backend/src/main/java/xyz/hidasy/rest/SoapeService.java
+++ backend/src/main/java/xyz/hidasy/rest/SoapeService.java
@@ -7,14 +7,13 @@ import java.sql.SQLException;
 /**
  * @author Caique Moreira caique.moreira@movile.com
  */
-@Path("/")
+@Path("/appointment")
 public class SoapeService {
 
     @GET
-    @Path("patient/{patientId}/appointment/{appointmentId}/subjective")
+    @Path("{appointmentId}/subjective")
     @Produces(MediaType.APPLICATION_JSON)
-    public SubjectiveResponse getSubjective(@PathParam("patientId") Long patientId,
-                                      @PathParam("appointmentId") Long appointmentId) {
+    public SubjectiveResponse getSubjective(@PathParam("appointmentId") Long appointmentId) {
         Subjective subjective = Database.getSubjectiveById(appointmentId);
         SubjectiveResponse subjectiveResponse = new SubjectiveResponse();
         subjectiveResponse.setSubjective(subjective);
@@ -30,11 +29,10 @@ public class SoapeService {
     }
 
     @POST
-    @Path("patient/{patientId}/appointment/{appointmentId}/subjective")
+    @Path("{appointmentId}/subjective")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public SubjectiveResponse addSubjective(@PathParam("patientId") Long patientId,
-                                    @PathParam("appointmentId") Long appointmentId,
+    public SubjectiveResponse addSubjective(@PathParam("appointmentId") Long appointmentId,
                                     Subjective subjective) throws SQLException {
         subjective.setAppointmentId(appointmentId);
 
@@ -53,5 +51,49 @@ public class SoapeService {
 
         return subjectiveResponse;
     }
+
+    /*@GET
+    @Path("patient/{patientId}/appointment/{appointmentId}/objective")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SubjectiveResponse getObjective(@PathParam("patientId") Long patientId,
+                                            @PathParam("appointmentId") Long appointmentId) {
+        Objective objective = Database.getObjectiveById(appointmentId);
+        ObjectiveResponse subjectiveResponse = new SubjectiveResponse();
+        subjectiveResponse.setSubjective(objective);
+        if (objective != null) {
+            subjectiveResponse.setStatusId(0);
+            subjectiveResponse.setStatusMessage("Subjective found");
+        } else {
+            subjectiveResponse.setStatusId(1);
+            subjectiveResponse.setStatusMessage("Subjective not found");
+        }
+
+        return subjectiveResponse;
+    }
+
+    @POST
+    @Path("patient/{patientId}/appointment/{appointmentId}/subjective")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public SubjectiveResponse addSubjective(@PathParam("patientId") Long patientId,
+                                            @PathParam("appointmentId") Long appointmentId,
+                                            Subjective subjective) throws SQLException {
+        subjective.setAppointmentId(appointmentId);
+
+        SubjectiveResponse subjectiveResponse = new SubjectiveResponse();
+        subjectiveResponse.setSubjective(subjective);
+
+        try {
+            Database.addSubjective(subjective);
+            subjectiveResponse.setStatusId(0);
+            subjectiveResponse.setStatusMessage("Subjective inserted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            subjectiveResponse.setStatusId(1);
+            subjectiveResponse.setStatusMessage("Subjective not inserted. Error: " + e.getMessage());
+        }
+
+        return subjectiveResponse;
+    }*/
 
 }
