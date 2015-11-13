2c3b49f2ad332dd44d2736ad846d9d02174f36bc
Gabriel Hidasy <gabrielhidasy@gmail.com>
2015-11-13 21:56:12 -0200

    Adding new controllers

---
+++ backend/src/main/webapp/js/controllers/DiagnosisController.js
@@ -0,0 +1,26 @@
+app.controller('DiagnosisController', ['$scope', 'soapeService', function($scope, soapeService) {
+
+  $scope.diagnosis = {appointmentId : 1};
+  $scope.success = false;
+  $scope.fail = false;
+
+  $scope.insertDiagnosis = function(diagnosis) {
+      $scope.success = false;
+      $scope.fail = false;
+      console.log(diagnosis);
+	  soapeService.insertDiagnosis(diagnosis).success(function(response) {
+          if (response.statusId == 0) {
+              $scope.diagnosis = {};
+              $scope.success = true;
+          } else {
+              $scope.fail = true;
+              console.log(response);
+              $scope.errorMessage = response.statusMessage;
+          }
+	  })
+	  .error(function(err) {
+          $scope.fail = true;
+          $scope.errorMessage = err;
+	  });
+  };
+}]);
